34be5b65b0c6086c096a16bed4657857b86e7979
lingbao <852347376@qq.com>
2017-12-01 17:25:22 +0800

    扫一扫

--- SharedParking/Class/ParkingSpace(车位)/Controller/ParkingSpaceVC.m
+++ SharedParking/Class/ParkingSpace(车位)/Controller/ParkingSpaceVC.m
@@ -7,6 +7,8 @@
 //
 
 #import "ParkingSpaceVC.h"
+#import <AVFoundation/AVFoundation.h>
+#import "SGQRCodeScanningVC.h"
 #import "CarportPayVC.h"
 #import "CarportOpenVC.h"
 
@@ -81,8 +83,52 @@
 #pragma mark ---------------action ---------------------/
 - (void)codeAction{
 //    CarportPayVC *vc = [[CarportPayVC alloc]init];
-    CarportOpenVC *vc = [[CarportOpenVC alloc]init];
-    [self.navigationController pushViewController:vc animated:YES];
+//    CarportOpenVC *vc = [[CarportOpenVC alloc]init];
+//    [self.navigationController pushViewController:vc animated:YES];
+    
+    // 1、 获取摄像设备
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    if (device) {
+        AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+        if (status == AVAuthorizationStatusNotDetermined) {
+            [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+                if (granted) {
+                    dispatch_sync(dispatch_get_main_queue(), ^{
+                        SGQRCodeScanningVC *vc = [[SGQRCodeScanningVC alloc] init];
+                        [self.navigationController pushViewController:vc animated:YES];
+                    });
+                    // 用户第一次同意了访问相机权限
+                    NSLog(@"用户第一次同意了访问相机权限 - - %@", [NSThread currentThread]);
+                    
+                } else {
+                    // 用户第一次拒绝了访问相机权限
+                    NSLog(@"用户第一次拒绝了访问相机权限 - - %@", [NSThread currentThread]);
+                }
+            }];
+        } else if (status == AVAuthorizationStatusAuthorized) { // 用户允许当前应用访问相机
+            SGQRCodeScanningVC *vc = [[SGQRCodeScanningVC alloc] init];
+            [self.navigationController pushViewController:vc animated:YES];
+        } else if (status == AVAuthorizationStatusDenied) { // 用户拒绝当前应用访问相机
+            UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"请去-> [设置 - 隐私 - 相机 - SGQRCodeExample] 打开访问开关" preferredStyle:(UIAlertControllerStyleAlert)];
+            UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+                
+            }];
+            
+            [alertC addAction:alertA];
+            [self presentViewController:alertC animated:YES completion:nil];
+            
+        } else if (status == AVAuthorizationStatusRestricted) {
+            NSLog(@"因为系统原因, 无法访问相册");
+        }
+    } else {
+        UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"未检测到您的摄像头" preferredStyle:(UIAlertControllerStyleAlert)];
+        UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+            
+        }];
+        
+        [alertC addAction:alertA];
+        [self presentViewController:alertC animated:YES completion:nil];
+    }
     
 }
 
---
+++ SharedParking/Class/ParkingSpace(车位)/QRCode(扫一扫)/Controller/SGQRCodeScanningVC.m
@@ -0,0 +1,293 @@
+//
+//  SGQRCodeScanningVC.m
+//  SGQRCodeExample
+//
+//  Created by kingsic on 17/3/20.
+//  Copyright © 2017年 kingsic. All rights reserved.
+//
+
+#import "SGQRCodeScanningVC.h"
+#import "SGQRCode.h"
+
+
+@interface SGQRCodeScanningVC () <SGQRCodeScanManagerDelegate, SGQRCodeAlbumManagerDelegate>
+@property (nonatomic, strong) SGQRCodeScanManager *manager;
+@property (nonatomic, strong) SGQRCodeScanningView *scanningView;
+@property (nonatomic, strong) UIButton *flashlightBtn;
+@property (nonatomic, strong) UILabel *promptLabel;
+@property (nonatomic, assign) BOOL isSelectedFlashlightBtn;
+@property (nonatomic, strong) UIView *bottomView;
+#pragma mark ---------------galaxy add ---------------------/
+@property (nonatomic , strong) UIView *focusView;
+@end
+
+@implementation SGQRCodeScanningVC
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self.scanningView addTimer];
+    [_manager resetSampleBufferDelegate];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self.scanningView removeTimer];
+    [self removeFlashlightBtn];
+    [_manager cancelSampleBufferDelegate];
+}
+
+- (void)dealloc {
+    NSLog(@"SGQRCodeScanningVC - dealloc");
+    [self removeScanningView];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    self.view.backgroundColor = [UIColor clearColor];
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    [self.view addSubview:self.scanningView];
+    [self setupNavigationBar];
+    [self setupQRCodeScanning];
+    [self.view addSubview:self.promptLabel];
+    /// 为了 UI 效果
+    [self.view addSubview:self.bottomView];
+}
+
+- (void)setupNavigationBar {
+    self.title = @"扫一扫";
+//    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"相册" style:(UIBarButtonItemStyleDone) target:self action:@selector(rightBarButtonItenAction)];
+}
+
+- (SGQRCodeScanningView *)scanningView {
+    if (!_scanningView) {
+        _scanningView = [[SGQRCodeScanningView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 0.9 * self.view.frame.size.height)];
+//        _scanningView.scanningImageName = @"SGQRCode.bundle/QRCodeScanningLineGrid";
+//        _scanningView.scanningAnimationStyle = ScanningAnimationStyleGrid;
+//        _scanningView.cornerColor = [UIColor orangeColor];
+        
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapFocusing:)];
+        //单点触摸
+        tap.numberOfTouchesRequired = 1;
+        //点击几次，如果是1就是单击
+        tap.numberOfTapsRequired = 1;
+        [_scanningView addGestureRecognizer:tap];
+        
+        UITapGestureRecognizer *tap2 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleTop:)];
+        tap2.numberOfTapsRequired = 2;
+        [_scanningView addGestureRecognizer:tap2];
+        //如果满足第二次 第一次的就取消
+        [tap requireGestureRecognizerToFail:tap2];
+    }
+    return _scanningView;
+}
+- (void)removeScanningView {
+    [self.scanningView removeTimer];
+    [self.scanningView removeFromSuperview];
+    self.scanningView = nil;
+}
+
+- (void)rightBarButtonItenAction {
+    SGQRCodeAlbumManager *manager = [SGQRCodeAlbumManager sharedManager];
+    [manager readQRCodeFromAlbumWithCurrentController:self];
+    manager.delegate = self;
+
+    if (manager.isPHAuthorization == YES) {
+        [self.scanningView removeTimer];
+    }
+}
+
+- (void)setupQRCodeScanning {
+    self.manager = [SGQRCodeScanManager sharedManager];
+    NSArray *arr = @[AVMetadataObjectTypeQRCode, AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode128Code];
+    // AVCaptureSessionPreset1920x1080 推荐使用，对于小型的二维码读取率较高
+    [_manager setupSessionPreset:AVCaptureSessionPreset1920x1080 metadataObjectTypes:arr currentController:self];
+//    [manager cancelSampleBufferDelegate];
+    _manager.delegate = self;
+}
+
+#pragma mark - - - SGQRCodeAlbumManagerDelegate
+- (void)QRCodeAlbumManagerDidCancelWithImagePickerController:(SGQRCodeAlbumManager *)albumManager {
+    [self.view addSubview:self.scanningView];
+}
+- (void)QRCodeAlbumManager:(SGQRCodeAlbumManager *)albumManager didFinishPickingMediaWithResult:(NSString *)result {
+    if ([result hasPrefix:@"http"]) {
+
+    } else {
+
+    }
+}
+
+#pragma mark - - - SGQRCodeScanManagerDelegate
+- (void)QRCodeScanManager:(SGQRCodeScanManager *)scanManager didOutputMetadataObjects:(NSArray *)metadataObjects {
+    NSLog(@"metadataObjects - - %@", metadataObjects);
+    if (metadataObjects != nil && metadataObjects.count > 0) {
+        [scanManager palySoundName:@"SGQRCode.bundle/sound.caf"];
+        [scanManager stopRunning];
+        [scanManager videoPreviewLayerRemoveFromSuperlayer];
+        
+        AVMetadataMachineReadableCodeObject *obj = metadataObjects[0];
+//        ScanSuccessJumpVC *jumpVC = [[ScanSuccessJumpVC alloc] init];
+//        jumpVC.jump_URL = [obj stringValue];
+//        [self.navigationController pushViewController:jumpVC animated:YES];
+    } else {
+        NSLog(@"暂未识别出扫描的二维码");
+    }
+}
+- (void)QRCodeScanManager:(SGQRCodeScanManager *)scanManager brightnessValue:(CGFloat)brightnessValue {
+    if (brightnessValue < - 1) {
+        [self.view addSubview:self.flashlightBtn];
+    } else {
+        if (self.isSelectedFlashlightBtn == NO) {
+            [self removeFlashlightBtn];
+        }
+    }
+}
+
+- (UILabel *)promptLabel {
+    if (!_promptLabel) {
+        _promptLabel = [[UILabel alloc] init];
+        _promptLabel.backgroundColor = [UIColor clearColor];
+        CGFloat promptLabelX = 0;
+        CGFloat promptLabelY = 0.73 * self.view.frame.size.height;
+        CGFloat promptLabelW = self.view.frame.size.width;
+        CGFloat promptLabelH = 35;
+        _promptLabel.frame = CGRectMake(promptLabelX, promptLabelY, promptLabelW, promptLabelH);
+        _promptLabel.textAlignment = NSTextAlignmentCenter;
+        _promptLabel.font = [UIFont boldSystemFontOfSize:13.0];
+        _promptLabel.textColor = [[UIColor whiteColor] colorWithAlphaComponent:0.6];
+        _promptLabel.text = @"将二维码/条码放入框内, 即可自动扫描\n双击可放大";
+        _promptLabel.numberOfLines = 2;
+    }
+    return _promptLabel;
+}
+
+- (UIView *)bottomView {
+    if (!_bottomView) {
+        _bottomView = [[UIView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.scanningView.frame), self.view.frame.size.width, self.view.frame.size.height - CGRectGetMaxY(self.scanningView.frame))];
+        _bottomView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.5];
+    }
+    return _bottomView;
+}
+
+#pragma mark - - - 闪光灯按钮
+- (UIButton *)flashlightBtn {
+    if (!_flashlightBtn) {
+        // 添加闪光灯按钮
+        _flashlightBtn = [UIButton buttonWithType:(UIButtonTypeCustom)];
+        CGFloat flashlightBtnW = 30;
+        CGFloat flashlightBtnH = 30;
+        CGFloat flashlightBtnX = 0.5 * (self.view.frame.size.width - flashlightBtnW);
+        CGFloat flashlightBtnY = 0.55 * self.view.frame.size.height;
+        _flashlightBtn.frame = CGRectMake(flashlightBtnX, flashlightBtnY, flashlightBtnW, flashlightBtnH);
+        [_flashlightBtn setBackgroundImage:[UIImage imageNamed:@"SGQRCodeFlashlightOpenImage"] forState:(UIControlStateNormal)];
+        [_flashlightBtn setBackgroundImage:[UIImage imageNamed:@"SGQRCodeFlashlightCloseImage"] forState:(UIControlStateSelected)];
+        [_flashlightBtn addTarget:self action:@selector(flashlightBtn_action:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _flashlightBtn;
+}
+
+- (void)flashlightBtn_action:(UIButton *)button {
+    if (button.selected == NO) {
+        [SGQRCodeHelperTool SG_openFlashlight];
+        self.isSelectedFlashlightBtn = YES;
+        button.selected = YES;
+    } else {
+        [self removeFlashlightBtn];
+    }
+}
+
+- (void)removeFlashlightBtn {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [SGQRCodeHelperTool SG_CloseFlashlight];
+        self.isSelectedFlashlightBtn = NO;
+        self.flashlightBtn.selected = NO;
+        [self.flashlightBtn removeFromSuperview];
+    });
+}
+#pragma mark ---------------galaxy add ---------------------/
+
+- (void)tapFocusing:(UITapGestureRecognizer *)sender{
+    
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    if (!device)
+        return;
+    //    [self captureStillImage];
+    
+    NSError *error = nil;
+    [device lockForConfiguration:&error];
+    
+    CGPoint point = [sender locationInView:self.scanningView];
+    NSLog(@"handleSingleTap!pointx:%f,y:%f",point.x,point.y);
+    CGSize size = self.view.bounds.size;
+    CGPoint focusPoint = CGPointMake(point.y /size.height ,1-point.x/size.width);
+    //对焦模式和对焦点
+    if ([device isFocusModeSupported:AVCaptureFocusModeAutoFocus]) {
+        [device setFocusPointOfInterest:focusPoint];
+        [device setFocusMode:AVCaptureFocusModeAutoFocus];
+    }else{
+        NSLog(@"聚焦模式修改失败");
+    }
+    
+    [device unlockForConfiguration];
+    
+    //设置对焦动画
+    _focusView.center = point;
+    _focusView.hidden = NO;
+    [UIView animateWithDuration:0.3 animations:^{
+        _focusView.transform = CGAffineTransformMakeScale(1.25, 1.25);
+    }completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.5 animations:^{
+            _focusView.transform = CGAffineTransformIdentity;
+        } completion:^(BOOL finished) {
+            _focusView.hidden = YES;
+        }];
+    }];
+}
+
+- (void)doubleTop:(UITapGestureRecognizer *)sender{
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    if (!device)
+        return;
+    
+    NSError *error = nil;
+    [device lockForConfiguration:&error];
+    
+    if (!error) {
+        if (device.videoZoomFactor == 1) {
+            device.videoZoomFactor = 6;
+        }else{
+            device.videoZoomFactor = 1;
+        }
+        DLog(@"-----------------%f",device.videoZoomFactor);
+        //聚焦
+        if ([device isFocusModeSupported:AVCaptureFocusModeContinuousAutoFocus]) {
+            [device setFocusMode:AVCaptureFocusModeContinuousAutoFocus];
+        }else{
+            NSLog(@"聚焦模式修改失败");
+        }
+        [device unlockForConfiguration];
+    }
+    
+}
+
+
+//对焦框
+- (UIView *)focusView{
+    if (! _focusView) {
+        _focusView = [[UIView alloc]init];
+        _focusView.layer.borderColor = kNavBarColor.CGColor;
+        _focusView.layer.borderWidth = 1.0;
+        _focusView.layer.cornerRadius = 5;
+        _focusView.layer.masksToBounds = YES;
+        _focusView.hidden = YES;
+        _focusView.size = CGSizeMake(80, 80);
+    }
+    return _focusView;
+}
+
+@end
+
